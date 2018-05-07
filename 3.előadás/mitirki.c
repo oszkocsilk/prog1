7e6d311be2287591f8f5aeb19a6a00f2ace957b2
oszkocsilk <oszkocsilk@gmail.com>
2018-05-07 20:00:36 +0200

    Add files via upload

---
+++ 3.előadás/mitirki.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int void1()
+{}
+int void2(void)
+{}
+int main()
+{
+	void1();
+	void2();
+	void1(5);
+	void2(5);
+
+
+
+
+return 0;}
---
+++ 3.előadás/sizeofchar.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void){
+
+
+	printf("%d\n", printf ("%d\n", printf ("%d\n", sizeof (char) * 8)));
+	return 0;
+}
